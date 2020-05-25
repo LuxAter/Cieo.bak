46f54d8c6e0ccb299b4ab9d62eb806dd2c2b281f
Arden Rasmussen <ardenisthebest@gmail.com>
2020-05-25 14:25:31 -0700

    Removed bad documentation

--- cieo/include/cieo/os.hpp
+++ cieo/include/cieo/os.hpp
@@ -1,57 +1,13 @@
-/**
- * @file os.hpp
- * @brief Os detection
- * @author Arden Rasmussen
- * @version 1.0
- * @date 2020-03-30
- * @ingroup global global-os
- */
 #ifndef CIEO_OS_DETECTION_H
 #define CIEO_OS_DETECTION_H
-
-/**
- * @defgroup global-os Global OS
- * @ingroup global
- *
- * Preprocessor definitions that are used to detect the operating system at
- * compile time.
- */
-
-/**
- * @ingroup global-os
- */
 #define CIEO_OS_IS_Windows 0
-/**
- * @ingroup global-os
- */
 #define CIEO_OS_IS_Android 0
-/**
- * @ingroup global-os
- */
 #define CIEO_OS_IS_Linux 0
-/**
- * @ingroup global-os
- */
 #define CIEO_OS_IS_BSD 0
-/**
- * @ingroup global-os
- */
 #define CIEO_OS_IS_HP_UX 0
-/**
- * @ingroup global-os
- */
 #define CIEO_OS_IS_AIX 0
-/**
- * @ingroup global-os
- */
 #define CIEO_OS_IS_IOS 0
-/**
- * @ingroup global-os
- */
 #define CIEO_OS_IS_OSX 0
-/**
- * @ingroup global-os
- */
 #define CIEO_OS_IS_Solaris 0
 
 #if defined(_WIN32) || defined(_WIN64) || defined(__CYGWIN__)
